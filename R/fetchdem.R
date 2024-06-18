eecce320e43c0ccbaafb9b1d0e71b8bd800cbe87
Emmanuel Teitelbaum <emmanuel.teitelbaum@gmail.com>
2024-06-17 20:33:33 -0400

    Add examples for fetchdem

--- R/fetchdem.R
+++ R/fetchdem.R
@@ -1,7 +1,6 @@
 #' Fetch and Filter V-Dem Lite Data
 #'
-#' This function fetches and filters data from the V-Dem Lite dataset based on specified indicators, years, and countries.
-#' It can return the data in either wide or long format.
+#' This function fetches and filters data from the V-Dem Lite dataset based on specified indicators, years, and countries. It can return the data in either wide or long format.
 #'
 #' @param indicators A character vector of indicator names or categories. If NULL, all indicators are included.
 #' @param start_year The starting year for filtering the data. Default is 1970.
@@ -11,8 +10,41 @@
 #' @return A data frame containing the filtered V-Dem Lite data in the specified format.
 #' @import dplyr tidyr
 #' @examples
-#' fetchdem(indicators = c("v2x_polyarchy", "v2x_freexp_altinf"), start_year = 2000, end_year = 2020, countries = c("USA", "SWE"), wide = TRUE)
-#' fetchdem(indicators = "v2x_polyarchy", start_year = 1980, end_year = 2020, countries = NULL, wide = FALSE)
+#' # Polyarchy score for all countries for 1980-2020
+#' polyarchy <- fetchdem(indicators = "v2x_polyarchy",
+#'                       start_year = 1980, end_year = 2020)
+#'
+#' # Polyarchy and clean elections index for USA and Sweden for 2000-2020
+#' my_indicators <- fetchdem(indicators = c("v2x_polyarchy", "v2xel_frefair"),
+#'                         start_year = 2000, end_year = 2020,
+#'                         countries = c("USA", "SWE"))
+#'
+#' # Five main high-level indices for all countries and years in long format
+#' big_5 <- fetchdem(indicators = "big_5", wide = FALSE)
+#'
+#' # Indicators that comprise the liberal democracy index for India, all years
+#' lib_dem_ind_2010 <- fetchdem(indicators = "v2x_libdem_all",
+#'                             countries = "IND")
+#'
+#' # Indicators that comprise the Regimes of the World index for Venezuela, all years
+#' regime_venezuela <- fetchdem(indicators = "v2x_regime_all",
+#'                            countries = "VEN")
+#'
+#' # All high-level indices for all countries in 2015
+#' high_level_2015 <- fetchdem(indicators = "high_level",
+#'                         start_year = 2015, end_year = 2015)
+#'
+#' # All mid-level indices for all countries in 2015
+#' mid_level_2015 <- fetchdem(indicators = "mid_level",
+#'                          start_year = 2015, end_year = 2015)
+#'
+#' # All lower-level indices for all countries in 2015
+#' low_level_2015 <- fetchdem(indicators = "lower_level",
+#'                         start_year = 2015, end_year = 2015)
+#'
+#' # Polyarchy and background factors, all countries and years
+#' polyarchy_analysis <- fetchdem(indicators = c("v2x_polyarchy", "background"))
+#'
 #' @export
 fetchdem <- function(indicators = NULL,
                      start_year = 1970,
@@ -80,7 +112,9 @@ fetchdem <- function(indicators = NULL,
       dplyr::select(c("country_name",
                       "country_text_id",
                       "year",
-                      all_of(indicators)))
+                      # some indicators from codebook not available in vdem,
+                      # so we use `any_of` to avoid errors
+                      any_of(indicators)))
   }
 
   # Reshape data to wide or long format based on the user's choice

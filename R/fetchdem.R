fde3bd962fbd59dacb25ea4b78e2d79c70a0daad
Emmanuel Teitelbaum <emmanuel.teitelbaum@gmail.com>
2024-06-19 23:53:20 -0400

    add summarize_dem()

--- R/fetchdem.R
+++ R/fetchdem.R
@@ -1,6 +1,6 @@
 #' Fetch and Filter V-Dem Lite Data
 #'
-#' This function fetches and filters data from the V-Dem Lite dataset based on specified indicators, years, and countries. It can return the data in either wide or long format.
+#' This function filters data from a slimmed down version of the V-Dem dataset based on specified indicators, years, and countries. It can return the data in either wide or long format. Users can query specific indices and indicators as well as pre-defined groups of indicators as defined by the V-Dem codebook.
 #'
 #' @param indicators A character vector of indicator names or categories. If NULL, all indicators are included.
 #' @param start_year The starting year for filtering the data. Default is 1970.
@@ -11,49 +11,46 @@
 #' @import dplyr tidyr
 #' @examples
 #' # Polyarchy score for all countries for 1980-2020
-#' polyarchy <- fetchdem(indicators = "v2x_polyarchy",
+#' polyarchy <- fetch_dem(indicators = "v2x_polyarchy",
 #'                       start_year = 1980, end_year = 2020)
 #'
 #' # Polyarchy and clean elections index for USA and Sweden for 2000-2020
-#' my_indicators <- fetchdem(indicators = c("v2x_polyarchy", "v2xel_frefair"),
+#' my_indicators <- fetch_dem(indicators = c("v2x_polyarchy", "v2xel_frefair"),
 #'                         start_year = 2000, end_year = 2020,
 #'                         countries = c("USA", "SWE"))
 #'
 #' # Five main high-level indices for all countries and years in long format
-#' big_5 <- fetchdem(indicators = "big_5", wide = FALSE)
+#' big_5 <- fetch_dem(indicators = "big_5", wide = FALSE)
 #'
 #' # Indicators that comprise the liberal democracy index for India, all years
-#' lib_dem_ind_2010 <- fetchdem(indicators = "v2x_libdem_all",
+#' lib_dem_ind_2010 <- fetch_dem(indicators = "v2x_libdem_all",
 #'                             countries = "IND")
 #'
 #' # Indicators that comprise the Regimes of the World index for Venezuela, all years
-#' regime_venezuela <- fetchdem(indicators = "v2x_regime_all",
+#' regime_venezuela <- fetch_dem(indicators = "v2x_regime_all",
 #'                            countries = "VEN")
 #'
 #' # All high-level indices for all countries in 2015
-#' high_level_2015 <- fetchdem(indicators = "high_level",
+#' high_level_2015 <- fetch_dem(indicators = "high_level",
 #'                         start_year = 2015, end_year = 2015)
 #'
 #' # All mid-level indices for all countries in 2015
-#' mid_level_2015 <- fetchdem(indicators = "mid_level",
+#' mid_level_2015 <- fetch_dem(indicators = "mid_level",
 #'                          start_year = 2015, end_year = 2015)
 #'
 #' # All lower-level indices for all countries in 2015
-#' low_level_2015 <- fetchdem(indicators = "lower_level",
+#' low_level_2015 <- fetch_dem(indicators = "lower_level",
 #'                         start_year = 2015, end_year = 2015)
 #'
 #' # Polyarchy and background factors, all countries and years
-#' polyarchy_analysis <- fetchdem(indicators = c("v2x_polyarchy", "background"))
+#' polyarchy_analysis <- fetch_dem(indicators = c("v2x_polyarchy", "background"))
 #'
 #' @export
-fetchdem <- function(indicators = NULL,
+fetch_dem <- function(indicators = NULL,
                      start_year = 1970,
                      end_year = 2023,
                      countries = NULL,
                      wide = TRUE) {
-
-  # Load lookup table
-  lookup_tbl <- lookup_tbl
 
   if (!is.null(indicators)) {
     # Initialize expanded indicators
@@ -109,12 +106,12 @@ fetchdem <- function(indicators = NULL,
   # Select indicators if provided
   if (!is.null(indicators)) {
     filtered_data <- filtered_data |>
-      dplyr::select(c("country_name",
-                      "country_text_id",
-                      "year",
-                      # some indicators from codebook not available in vdem,
-                      # so we use `any_of` to avoid errors
-                      any_of(indicators)))
+      dplyr::select(country_name,
+                    country_text_id,
+                    year,
+                    # some indicators from codebook not available in vdem,
+                    # so we use `any_of` to avoid errors
+                    any_of(indicators))
   }
 
   # Reshape data to wide or long format based on the user's choice
---
+++ R/summarizedem.R
@@ -0,0 +1,137 @@
+#' Summarize V-Dem Data by Country
+#'
+#' This function summarizes a specified V-Dem indicator by country for a given time period.
+#'
+#' @param indicator A character string specifying the V-Dem indicator to summarize.
+#' @param start_year An integer specifying the start year for the summary (default is 1970).
+#' @param end_year An integer specifying the end year for the summary (default is 2023).
+#' @param countries An optional character vector specifying the countries to include (default is NULL, which includes all countries).
+#' @param visualization An optional character string specifying the type of visualization to include in the summary table (default is NULL, which includes no visualization). Options include "sparkline", "density", or "histogram". Note that it is not recommended to set the visualization parameter if the number of selected countries is large, as it may take a long time to render the table.
+#' @return A table summarizing the specified indicator by country.
+#' @examples
+#' summarize_dem(indicator = "v2x_polyarchy", start_year = 2000, end_year = 2020)
+#' @import dplyr gt gtExtras
+#' @importFrom stats median sd
+#' @importFrom rlang sym !!
+#' @export
+summarize_dem <- function(indicator = NULL,
+                          start_year = 1970,
+                          end_year = 2023,
+                          countries = NULL,
+                          visualization = NULL) {
+
+  # Check if indicator is provided
+  if (is.null(indicator)) {
+    stop("Indicator cannot be NULL. Please provide one indicator.")
+  }
+
+  # Check if exactly one indicator is provided
+  if (length(indicator) != 1) {
+    stop("Please provide exactly one indicator.")
+  }
+
+  # Check if the provided indicator is in the lookup table
+  valid_indicators <- lookup_tbl$tag
+  if (!(indicator %in% valid_indicators)) {
+    stop("The provided indicator is not valid.")
+  }
+
+  # Filter the dataset by the specified years
+  filtered_data <- vdemlite |>
+    dplyr::filter(year >= start_year & year <= end_year)
+
+  # Filter by countries if provided
+  if (!is.null(countries)) {
+    filtered_data <- filtered_data |>
+      dplyr::filter(country_text_id %in% countries)
+  }
+
+  # Select the specified indicator along with year and country
+  filtered_data <- filtered_data |>
+    dplyr::select(country_name,
+                  country_text_id,
+                  year,
+                  !!rlang::sym(indicator))
+
+  # Calculate the mean indicator value for each year across all countries
+  yearly_means <- filtered_data |>
+    dplyr::group_by(year) |>
+    dplyr::summarize(mean_value = mean(!!rlang::sym(indicator), na.rm = TRUE)) |>
+    dplyr::pull(mean_value)
+
+  # Calculate summary statistics for all countries combined
+  overall_summary <- filtered_data |>
+    dplyr::summarize(
+      country_name = "All Selected Countries",
+      country_text_id = "",
+      unique = dplyr::n_distinct(!!rlang::sym(indicator), na.rm = TRUE),
+      missing_pct = mean(is.na(!!rlang::sym(indicator))) * 100,
+      mean = mean(!!rlang::sym(indicator), na.rm = TRUE),
+      sd = sd(!!rlang::sym(indicator), na.rm = TRUE),
+      min = min(!!rlang::sym(indicator), na.rm = TRUE),
+      median = median(!!rlang::sym(indicator), na.rm = TRUE),
+      max = max(!!rlang::sym(indicator), na.rm = TRUE),
+      indicator_data = list(yearly_means)
+    )
+
+  # Group by country and summarize the data
+  summary_by_country <- filtered_data |>
+    dplyr::group_by(country_name, country_text_id) |>
+    dplyr::summarize(
+      unique = dplyr::n_distinct(!!rlang::sym(indicator), na.rm = TRUE),
+      missing_pct = mean(is.na(!!rlang::sym(indicator))) * 100,
+      mean = mean(!!rlang::sym(indicator), na.rm = TRUE),
+      sd = sd(!!rlang::sym(indicator), na.rm = TRUE),
+      min = min(!!rlang::sym(indicator), na.rm = TRUE),
+      median = median(!!rlang::sym(indicator), na.rm = TRUE),
+      max = max(!!rlang::sym(indicator), na.rm = TRUE),
+      indicator_data = list(!!rlang::sym(indicator))
+    ) |>
+    dplyr::ungroup()
+
+  # Combine overall summary with country-specific summaries
+  combined_summary <- bind_rows(overall_summary, summary_by_country)
+
+  summary_table <- gt::gt(combined_summary) |>
+    gt::tab_header(
+      title = paste("Summary of", indicator, "by Country"),
+      subtitle = paste("Years:", start_year, "-", end_year)
+    ) |>
+    gt::fmt_number(decimals = 2) |>
+    gt::fmt_integer(unique) |>
+    gt::cols_label(
+      country_name = "Country",
+      country_text_id = "Country Code",
+      unique = "Unique",
+      missing_pct = "Missing (%)",
+      mean = "Mean",
+      sd = "Std. Dev.",
+      min = "Min",
+      median = "Median",
+      max = "Max"
+    ) |>
+    gt::opt_interactive(
+      use_search = TRUE,
+      use_compact_mode = TRUE,
+      use_page_size_select = TRUE
+      )
+
+  if (is.null(visualization)) {
+    return(summary_table |> cols_hide("indicator_data"))
+  } else if (visualization == "sparkline") {
+    return(summary_table |>
+             cols_label(indicator_data = "Sparkline") |>
+             gtExtras::gt_plt_sparkline(indicator_data))
+  } else if (visualization == "density") {
+    return(summary_table |>
+             cols_label(indicator_data = "Density Plot") |>
+             gtExtras::gt_plt_dist(indicator_data, type = "density"))
+  } else if (visualization == "histogram") {
+    return(summary_table |>
+             cols_label(indicator_data = "Histogram") |>
+             gtExtras::gt_plt_dist(indicator_data, type = "histogram"))
+    return(summary_table)
+  } else {
+    stop("Invalid visualization type. Please choose 'sparkline', 'density', or 'histogram'.")
+  }
+}
